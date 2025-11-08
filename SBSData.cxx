ccfac0eb9cde299b125e07c7172bbcf433b1d092
Edward Brash <brash@jlab.org>
2025-11-08 11:55:33 -0500

    btspaude_updates_nov2025

--- SBSData.cxx
+++ SBSData.cxx
@@ -81,7 +81,6 @@ namespace SBSData {
   void TDC::Process(Int_t elemID, Double_t val, Double_t fedge)
   {
     Int_t edge = int(fedge);
-    // std::cout << " tdc process " << val << " " << edge  << " ftdc hits size = " <<fTDC.hits.size() << " hits in edge "  << fEdgeIdx[edge]<< std::endl;
     if(edge < 0 || edge>1) {
       std::cerr << "Edge specified is not valid!" << std::endl;
       edge = 0;
@@ -108,6 +107,11 @@ namespace SBSData {
     }
     if(fEdgeIdx[1] > fEdgeIdx[0]) fEdgeIdx[0] = fEdgeIdx[1]; // if TE found first force LE count to increase
     fHasData = true;
+    if (hit->le.raw > 0 && hit->te.raw > 0 && hit->ToT.raw ==0) hit->ToT.raw = hit->te.raw - hit->le.raw;
+    if (hit->le.val > 0 && hit->te.val > 0 && hit->ToT.val ==0) hit->ToT.val = hit->te.val - hit->le.val;
+    
+	if (elemID == 2696) std::cout << " tdc process " << val << " " << edge  << " ftdc hits size = " <<fTDC.hits.size() << " hits in edge "  << fEdgeIdx[edge]<< " tdc LE " << hit->le.raw << " tdc TE " << hit->te.raw << " tdc TOT " << hit->ToT.raw << std::endl;
+    if (elemID == 2696) std::cout << " tdc process " << val << " " << edge  << " ftdc hits size = " <<fTDC.hits.size() << " hits in edge "  << fEdgeIdx[edge]<< " tdc LE val " << hit->le.val << " tdc TE val " << hit->te.val << " tdc TOT val" << hit->ToT.val << std::endl;
   }
 
   void TDC::Clear()
